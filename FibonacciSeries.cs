28dd7babd0bec3de1f675f576e6d397d19e93a06
LokeshKiran <lokeshkskiran@gmail.com>
2022-08-31 16:29:16 +0530

    [Lokesh]Added:Logical Programes 1.Fibonacci Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramesDay6
+{
+    internal class FibonacciSeries
+    {
+        public static void fibonacciSeries()
+        {
+            int a = 0,b=1,c=0;
+            Console.WriteLine("enter the range of Fibonacci Series:");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine("{0},\n{1}",a,b);
+            for(int i=2;i<n;i++)
+            {
+                c = a + b;
+                Console.WriteLine("{0}",c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramesDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("select choice:\n1.Fibonacci Series");
+            int choice=int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    FibonacciSeries.fibonacciSeries();
+                    break;
+                    default:
+                    Console.WriteLine("please enter the correct choice");
+                    break;
+            }
+        }
+    }
+}
